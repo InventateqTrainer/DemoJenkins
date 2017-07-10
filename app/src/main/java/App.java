23ba93a21f68fd44be7b6fbdcaf1910a78c626ba
InventateqTrainer <abhijit.skelkar@gmail.com>
2017-07-10 19:10:07 +0530

    Second Modification Bug #1234

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -4,7 +4,7 @@ import com.inventateq.Batch1.util.*;
 
 public class App {
  public static void main(String args[]){
-     System.out.println("Hello World !!! Today's Date is " + DateUtil.getToday());
+     System.out.println("Hello Wonderful World !!! Today's Date is " + DateUtil.getToday());
 
  }
 }
